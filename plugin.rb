955630eb6cb1989bc013715257955e8ad7759302
aathi <aathi@7zero.com>
2015-06-17 01:23:21 +0200

    commit - 1

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,14 @@
 # about: Display the topics as gallery
 # version: 0.1
 # authors: Selva Ganesh
+
+
+register_css <<CSS
+.wordpress-preferences-link {
+  margin-top: 32px;
+  margin-bottom: 24px;
+}
+.wordpress-preferences-link a {
+  text-decoration: underline;
+}
+CSS
