2a8b5f39b30649b94d4f1c0c7eb180d744e0e9aa
aathi <aathi@7zero.com>
2015-06-18 02:18:38 +0200

    changes - 2

--- plugin.rb
+++ plugin.rb
@@ -2,5 +2,6 @@
 # about: Display the topics as gallery
 # version: 0.1
 # authors: Selva Ganesh & Athithan
+# url: hhttps://github.com/Aathi/gallery-plugin
 
 register_asset "stylesheets/gallery.scss"
