caedc26a744eed3ed6836aac2b61126bbba93609
jstechiee <jstechiee@gmail.com>
2015-06-21 16:41:37 +0530

    Adds contact us page

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,32 @@
 # name: gallery
 # about: Display the topics as gallery
 # version: 0.1
-# authors: Selva Ganesh & Athithan
-# url: https://github.com/Aathi/gallery-plugin
+# authors: jstechiee & Athithan
 
 register_asset "stylesheets/gallery.scss"
+
+after_initialize do
+
+  module ::DiscourseGallery
+
+    class Engine < ::Rails::Engine
+      engine_name "discourse_gallery"
+      isolate_namespace DiscourseGallery
+    end
+
+  end
+
+  require_dependency "application_controller"
+  class DiscourseGallery::ContactusController < ::ApplicationController
+    def index
+    end
+  end
+
+  DiscourseGallery::Engine.routes.draw do
+    get '/' => 'contactus#index'
+  end
+
+  Discourse::Application.routes.append do
+    mount ::DiscourseGallery::Engine, at: "/contactus"
+  end
+end
